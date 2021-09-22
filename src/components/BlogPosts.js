4735e67d16195f0481fb733ca96ab1b03b6a107a
Alice <alice.moretti@truemedinc.com>
2021-09-22 15:18:10 +0300

    1st draft BlogPosts component for hp

---
+++ src/components/BlogPosts.js
@@ -0,0 +1,124 @@
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import styled from "styled-components"
+import { FiArrowRight } from "react-icons/fi"
+
+const query = graphql`
+  {
+    allContentfulBlog(sort: { fields: date, order: DESC }) {
+      nodes {
+        date(formatString: "MMMM Do, YYYY")
+        featured
+        id
+        title
+        topics
+        textPreview {
+          textPreview
+        }
+        picture {
+          gatsbyImageData(
+            layout: CONSTRAINED
+            placeholder: TRACED_SVG
+            width: 436
+            height: 369
+          )
+        }
+      }
+    }
+  }
+`
+
+const BlogPosts = () => {
+  const data = useStaticQuery(query)
+  const {
+    allContentfulBlog: { nodes: blogs },
+  } = data
+  console.log(blogs)
+
+  return (
+    <div>
+      {blogs.map(blog => {
+        const pathToImage = getImage(blog.picture)
+        return (
+          <Wrapper>
+            <article className="article-container">
+              <GatsbyImage
+                image={pathToImage}
+                alt="data travelling"
+              ></GatsbyImage>
+              <div className="text-container">
+                <div className="topic-container">
+                  {blog.topics.map(topic => {
+                    return <button className="btn-topic">{topic}</button>
+                  })}
+                </div>
+                <h1 className="title-style">{blog.title}</h1>
+                <div className="underline"></div>
+                <p>{blog.textPreview.textPreview}</p>
+
+                <div className="continue-reading-container">
+                  <p className="continue-reading-text">Continue reading</p>
+                  <button className="btn-arrow">
+                    <FiArrowRight />
+                  </button>
+                </div>
+                <footer className="date-container">
+                  <p className="date">{blog.date}</p>
+                </footer>
+              </div>
+            </article>
+          </Wrapper>
+        )
+      })}
+    </div>
+  )
+}
+
+const Wrapper = styled.section`
+  .article-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    margin-bottom: 70px;
+  }
+
+  .text-container {
+    padding: 1rem 2rem;
+  }
+
+  .topic-container {
+    display: flex;
+    margin-bottom: 1rem;
+  }
+
+  h1 {
+    color: hsl(206 54% 29%);
+    font-family: Lato;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 1.5rem;
+  }
+
+  .title-style {
+    margin-bottom: 0.8rem;
+  }
+  .underline {
+    width: 6rem;
+    height: 2px;
+    margin-left: 0px;
+    background: hsla(207, 20%, 80%, 0.5);
+    margin-bottom: 1rem;
+  }
+
+  .date-container {
+    border-top: 2px solid hsla(207, 20%, 80%, 0.5);
+    margin-top: 3.5rem;
+    padding-top: 0.5rem;
+  }
+
+  p {
+    font-size: 1rem;
+  }
+`
+
+export default BlogPosts
