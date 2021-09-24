0e6e5039176be886b6329e1e86c718d1614c778d
Alice <alice.moretti@truemedinc.com>
2021-09-24 11:32:46 +0300

    Created Title component and added to blogs page

---
+++ src/components/Title.js
@@ -0,0 +1,32 @@
+import React from "react"
+import styled from "styled-components"
+
+const Title = () => {
+  return (
+    <Wrapper>
+      <div className="title-container">
+        <h2 className="title-style">topics</h2>
+        <div className="line"></div>
+      </div>
+    </Wrapper>
+  )
+}
+const Wrapper = styled.section`
+  .title-container {
+    position: relative;
+    width: 200px;
+    margin: auto;
+    text-align: center;
+  }
+  .title-style {
+    font-family: Lato;
+    font-size: 36px;
+    background-color: #ffff;
+    padding: 0 0.5rem;
+    display: inline-block;
+    font-weight: normal;
+    text-transform: uppercase;
+    color: hsla(206, 54%, 29%, 1);
+  }
+`
+export default Title
--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/Layout"
 import Hero from "../components/Hero"
+import Title from "../components/Title"
 
 const Blogs = () => {
   return (
@@ -11,6 +12,9 @@ const Blogs = () => {
           subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
           blogsPic
         />
+        <div className="main-container-blogs">
+          <Title />
+        </div>
       </div>
     </Layout>
   )
