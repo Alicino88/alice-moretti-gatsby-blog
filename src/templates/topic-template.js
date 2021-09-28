8adb07ba06635ff97b6dc47be3c9e7febba2e88e
Alice <alice.moretti@truemedinc.com>
2021-09-28 19:23:35 +0300

    Drafted this page that is the one programmatically created by gatsby-node when a user clicks on the topic button

--- src/templates/topic-template.js
+++ src/templates/topic-template.js
@@ -1,7 +1,34 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-const TopicTemplate = () => {
+const TopicTemplate = props => {
+  console.log(props)
   return <div>test topic</div>
 }
-
+export const query = graphql`
+  query GetTopics($topic: String) {
+    allContentfulBlog(
+      sort: { fields: date, order: DESC }
+      filter: { topics: { eq: $topic } }
+    ) {
+      nodes {
+        date(formatString: "MMMM Do, YYYY")
+        id
+        title
+        topics
+        textPreview {
+          textPreview
+        }
+        picture {
+          gatsbyImageData(
+            layout: CONSTRAINED
+            placeholder: BLURRED
+            width: 436
+            height: 369
+          )
+        }
+      }
+    }
+  }
+`
 export default TopicTemplate
