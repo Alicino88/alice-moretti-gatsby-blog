c86791db4e6c79e3726b70128431eeac9e10f388
Alice <alice.moretti@truemedinc.com>
2021-09-28 20:06:47 +0300

    Added all components to topic-template.js and passed blogs prop to AllBlogPosts.js

--- src/templates/topic-template.js
+++ src/templates/topic-template.js
@@ -1,10 +1,36 @@
 import React from "react"
+import Layout from "../components/Layout"
+import Hero from "../components/Hero"
+import Title from "../components/Title"
+import Topics from "../components/Topics"
+import AllBlogPosts from "../components/AllBlogPosts"
 import { graphql } from "gatsby"
 
 const TopicTemplate = props => {
   console.log(props)
-  return <div>test topic</div>
+  const {
+    data: {
+      allContentfulBlog: { nodes: blogs },
+    },
+  } = props
+  return (
+    <Layout>
+      <div>
+        <Hero
+          title="all blogs"
+          subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+          blogsPic
+        />
+        <div className="main-container-blogs">
+          <Title />
+          <Topics />
+          <AllBlogPosts blogs={blogs} />
+        </div>
+      </div>
+    </Layout>
+  )
 }
+
 export const query = graphql`
   query GetTopics($topic: String) {
     allContentfulBlog(
