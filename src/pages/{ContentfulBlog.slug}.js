9f085e893c2813255e0652c4d0edd1331657d90c
Alice <alice.moretti@truemedinc.com>
2021-10-05 12:05:19 +0300

    Draft styled single blog

--- src/pages/{ContentfulBlog.slug}.js
+++ src/pages/{ContentfulBlog.slug}.js
@@ -2,6 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import { BLOCKS, MARKS } from "@contentful/rich-text-types"
 import { renderRichText } from "gatsby-source-contentful/rich-text"
+import styled from "styled-components"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 //when creating a page programmatically using gatsby file system route API
@@ -12,8 +13,17 @@ const SingleBlog = props => {
   //you need to pass to renderRichText function not the raw object itself but the piece that contains it!
   const content = props.data.contentfulBlog.content
 
+  //styling raw content:
+  const HeaderTwo = ({ children }) => <h2 className="h2-style">{children}</h2>
+  const Code = ({ children }) => <span className="code-style">{children}</span>
+
   const options = {
+    renderMark: {
+      [MARKS.CODE]: text => <Code>{text}</Code>,
+    },
     renderNode: {
+      [BLOCKS.HEADING_2]: (node, children) => <HeaderTwo>{children}</HeaderTwo>,
+
       [BLOCKS.EMBEDDED_ASSET]: node => {
         console.log(node.data.target.fixed.src)
         return <img src={node.data.target.fixed.src} />
@@ -21,11 +31,25 @@ const SingleBlog = props => {
     },
   }
 
-  //renderRichText takes the object containing the raw content and the options defining styles and additional
-  //assets such as pictures
+  //renderRichText takes as arguments the object containing the object with the raw content
+  //and the options defining styles and additional assets such as pictures
   const blog = renderRichText(content, options)
-
-  return <div>{blog}</div>
+  const title = props.data.contentfulBlog.title
+  const date = props.data.contentfulBlog.date
+  const pathToImage = getImage(props.data.contentfulBlog.picture)
+  return (
+    <Wrapper>
+      <div>
+        <div className="header-background"></div>
+        <article className="article-container">
+          <h1 className="title-style">{title}</h1>
+          <p className="paragraph-style">{date}</p>
+          <GatsbyImage className="blog-picture" image={pathToImage} />
+          <div>{blog}</div>
+        </article>
+      </div>
+    </Wrapper>
+  )
 }
 
 /*it seems that the key to fetch the images was to manually remove the children node from the query:
@@ -40,6 +64,9 @@ https://www.contentful.com/developers/docs/tutorials/general/rich-text-and-gatsb
 export const query = graphql`
   query getSingleBlog($slug: String) {
     contentfulBlog(slug: { eq: $slug }) {
+      picture {
+        gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
+      }
       title
       contentful_id
       date(formatString: "MMMM Do, YYYY")
@@ -62,6 +89,49 @@ export const query = graphql`
         }
       }
     }
+  }
+`
+
+const Wrapper = styled.section`
+  .header-background {
+    background-color: hsla(206, 20%, 80%, 0.5);
+    height: 400px;
+  }
+
+  .article-container {
+    //padding: 0 400px 0 400px;
+    width: 40vw;
+    margin: 0 auto;
+    margin-top: -250px;
+  }
+
+  .title-style {
+    border-left: 3px solid hsl(27, 41%, 72%);
+    padding-left: 5px;
+    font-size: 1.6rem;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+
+  .paragraph-style {
+    font-family: "Lato";
+    margin-bottom: 10px;
+    font-size: 0.8rem;
+  }
+
+  .blog-picture {
+    border-radius: 10px 10px 0 0;
+    height: 400px;
+  }
+  //Raw content style:
+  .h2-style {
+    color: hsl(206, 54%, 29%);
+    margin: 20px 0 20px 0;
+    font-family: Lato;
+  }
+
+  .code-style {
+    display: block;
   }
 `
 
