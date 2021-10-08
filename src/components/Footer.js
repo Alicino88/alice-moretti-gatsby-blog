7f1e54da4dd91e2236b5f797f00f01c47709384b
Alice <alice.moretti@truemedinc.com>
2021-10-08 13:15:24 +0300

    Added text to footer and changed height to auto

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,27 +5,42 @@ const Footer = () => {
   return (
     <Wrapper>
       <div className="footer-container">
-        Copyright&nbsp; <span> Alice Moretti </span>&nbsp; - 2021 | Built
-        with&nbsp;
-        <a href="https://www.gatsbyjs.com/" target="_blank" rel="noopener">
-          Gatsby.js
-        </a>
+        <p className="footer-content">
+          Copyright&nbsp; <span> Alice Moretti </span>&nbsp; - 2021 | Built
+          with&nbsp;
+          <a href="https://www.gatsbyjs.com/" target="_blank" rel="noopener">
+            Gatsby.js
+          </a>
+          &nbsp;and inspired by&nbsp;
+          <a
+            href="https://gatsby-mdx-blog-course-project.netlify.app/"
+            target="_blank"
+            rel="noopener"
+          >
+            MDX blog
+          </a>
+        </p>
       </div>
     </Wrapper>
   )
 }
 const Wrapper = styled.footer`
   .footer-container {
-    height: 80px;
+    height: auto;
     background-color: rgba(194, 205, 214, 0.5);
     display: flex;
     justify-content: center;
     align-items: center;
+    margin-top: 60px;
+  }
+
+  .footer-content {
     font-family: Lato, "Avant Garde", Sans-Serif;
     font-size: 0.9rem;
     font-weight: 300;
     color: hsla(0, 0%, 24%, 1);
-    margin-top: 60px;
+    text-align: center;
+    padding: 30px 20px 30px 20px;
   }
 
   span {
@@ -33,8 +48,6 @@ const Wrapper = styled.footer`
   }
 
   a {
-    text-decoration: none;
-    font-weight: bold;
     color: hsla(0, 0%, 24%, 1);
   }
 
